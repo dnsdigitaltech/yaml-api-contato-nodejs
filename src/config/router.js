620c4565e5105e50fb2e68864b5ec4df565a1dca
Davi Bernardo <dnssites@gmail.com>
2019-11-04 15:51:36 -0300

    Configurando rotas, criação e login de usuário ok

---
+++ src/config/router.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+module.exports = (app) => {
+  
+  app.use('/auth', app.routes.auth);
+  // api protegida dentre de um unico roteador
+  const protectedRouter = express.Router();
+  app.use('/', app.config.passport.authenticate(), protectedRouter);
+};
